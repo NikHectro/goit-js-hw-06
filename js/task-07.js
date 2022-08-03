971cdf3911fbe12e1c3acb3a77d65e2aabcb7494
NikHectro <99733904+NikHectro@users.noreply.github.com>
2022-08-03 20:10:44 +0300

    Task 7

--- js/task-07.js
+++ js/task-07.js
@@ -1,3 +1,12 @@
 "use strict"
 
-const 
+const rangeEl = document.querySelector("#font-size-control")
+console.log(rangeEl);
+const textEl = document.querySelector("#text");
+
+function changeSize(event) {
+    const rangeSize = event.target.value;
+    textEl.style.fontSize = rangeSize + "px";
+}
+
+rangeEl.addEventListener("input", changeSize)
