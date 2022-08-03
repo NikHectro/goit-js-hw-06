2ed6136c1474a739f9df66b721771e47f37a0192
NikHectro <99733904+NikHectro@users.noreply.github.com>
2022-08-03 18:39:45 +0300

    Task 6 experements

--- js/task-06.js
+++ js/task-06.js
@@ -6,8 +6,14 @@ const checkingLengs = inputEl.getAttribute("data-length");
 function handerInput(event) {
     const enterLength = event.target.value.length
     enterLength >= checkingLengs ?
-        inputEl.setAttribute("id", "validation-input.valid") :
-        inputEl.setAttribute("id", "validation-input.invalid");
+        inputEl.classList.remove("invalid");
+        inputEl.classList.add("valid") :
+        inputEl.classList.remove("valid");
+        inputEl.classList.add("invalid");
+        
+        // inputEl.setAttribute("id", "validation-input.valid") :
+        // inputEl.setAttribute("id", "validation-input.invalid");
+    
     // console.log(enterLength, checkingLengs);
     // console.log(enterLength >= checkingLengs);
     // console.log(inputEl.getAttribute("id"));
