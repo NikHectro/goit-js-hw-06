07e23afc7c02cb15e93c103ac131e90e40fa3338
NikHectro <99733904+NikHectro@users.noreply.github.com>
2022-08-03 21:45:17 +0300

    Task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyEl = document.querySelector("body");
+const colorNumber = document.querySelector(".color");
+const buttonEl = document.querySelector(".change-color");
+
+buttonEl.addEventListener("click", changeBgc);
+
+function changeBgc(event) {
+  const colorHex = getRandomHexColor();
+  bodyEl.style.backgroundColor = colorHex;
+  colorNumber.textContent = colorHex;
+}
